a24cd495a968f61fbc89e112a77d1cab86a862b2
MateuszKalenik01 <mkalenik@solvd.com>
2024-06-05 14:03:08 +0200

    MobileTests

--- src/main/java/com/solvd/pages/common/HomePageBase.java
+++ src/main/java/com/solvd/pages/common/HomePageBase.java
@@ -4,7 +4,6 @@ import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
--- src/main/java/com/solvd/pages/mobile/ProductPage.java
+++ src/main/java/com/solvd/pages/mobile/ProductPage.java
@@ -2,11 +2,7 @@ package com.solvd.pages.mobile;
 
 import com.solvd.pages.common.ProductPageBase;
 import com.zebrunner.carina.utils.factory.DeviceType;
-import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 @DeviceType(pageType = DeviceType.Type.ANDROID_PHONE,parentClass = ProductPageBase.class)
 public class ProductPage extends ProductPageBase {
